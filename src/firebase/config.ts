853a9b1f4a12f66e797d42dc16051bf2af9d5784
Dustin Myers <myersdigitalservicesai@gmail.com>
2026-02-03 04:55:55 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9280433457-99d31",
+  "appId": "1:465005760707:web:b9aa64a65fcd34a8fc9276",
+  "apiKey": "AIzaSyAeJvWSTeYLeJ9d1nSb9b0w-6AtaFnHNNY",
+  "authDomain": "studio-9280433457-99d31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "465005760707"
+};
